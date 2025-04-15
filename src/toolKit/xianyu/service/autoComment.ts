db5277f0134f04208c4e076250d6161dc8113f7a
yaoyc <yaoyunchou@bananain.com>
2025-04-15 14:20:42 +0800

    feat(商品信息): ✨ 添加获取商品信息的功能并优化自动评论逻辑

--- src/toolKit/xianyu/service/autoComment.ts
+++ src/toolKit/xianyu/service/autoComment.ts
@@ -5,7 +5,8 @@
  * 
  */
 import { Record } from "../../../lib/logger";
-import { APPNAME, findPage, setRunInfo } from "./base";
+import { getGoodInfo, getGoodInfoByOrderNumber } from "../../../lib/service";
+import { APPNAME, backPageByName, findPage, setRunInfo } from "./base";
 import { backMainPage, closeMainPopup, taskList } from "./getMainPopup";
 
 // 当前页同一个逻辑的循环最大次数
@@ -20,20 +21,7 @@ let maxLoopMap:any = {
 }
 
 
-/**
- * 一直回退到闲鱼页面
- */
-
-export function xianyuBack(){
-    // 获取当前页面的报名
-    launch(APPNAME);
-    sleep(2000)
-    // 检查
-    const name = currentPackage();
-    if(name !== APPNAME){
-        xianyuBack()
-    }
-}
+let maxErrorCount = 0
 
 /**
  * 1. 金币兑换
@@ -41,17 +29,151 @@ export function xianyuBack(){
  */
 export function startAutoComment() {
     try {
+        // 检查流程错误次数
+        if(maxErrorCount > 40){
+            // 重置所有错误
+            console.log('错误次数过多，出现死循环了')
+            return
+        }
         // 查看列表
-        // 检查当前页面和app
+        const list = className("android.widget.ImageView").descContains("闲鱼号").find()
+        // 循环list
+        for(let i = 0; i < list.length; i++){
+            const item = list[i]
+            // 获取内容
+            const contentDescription = item.contentDescription + ''
+            
+            console.log("昵称===================", contentDescription)
+            // 寻找父级
+            const parent = item.parent()
+            if(!parent){
+                maxErrorCount++
+                continue
+                
+            }
+            // 点击parent的中间， 进入商品详情
+            const parentReact = parent.bounds()
+            click(parentReact.centerX(), parentReact.centerY())
+            sleep(1000)
+            // 获取商品详情
+            const goodsDetail = className("android.view.View").descContains("订单编号").findOne()
+            if(!goodsDetail){
+                maxErrorCount++
+                continue
+            }
+            // 获取订单编号
+            const orderText = goodsDetail.contentDescription + ''
+            // 从商品详情中提取订单编号
+            const orderNumberMatch = orderText.match(/订单编号\s*(\d+)/);
+            if (!orderNumberMatch) {
+                maxErrorCount++
+                continue
+            }
+            // 获取订单编号
+            const orderNumber = orderNumberMatch[1]
+            console.log("订单编号===================", orderNumber)
+
+           
+            
+
+            // 处理数据
+            if(orderNumber){
+                // 通过昵称和标题， 获取对应的商品信息
+               
+                const goodsInfo =  getGoodInfoByOrderNumber(orderNumber)
+                console.log("商品信息===================", goodsInfo)
+                // 获取评价
+                if(goodsInfo.code === 0){
+                    try {
+                        // 获取评价
+                        const comment = goodsInfo.data.items[0].fields["评价"].value[0].text
+                        console.log("评价===================", comment)
+                        // 进入好评弹框入口
+                        const goodCommentPopup =className("android.view.View").descContains("按钮, 去评价").findOne(1000)
+                        // 获取他的位置信息
+                        const react = goodCommentPopup.bounds()
+                        console.log("react===================", react, react.centerX(), react.centerY())
+                        // 模拟点击对应的坐标
+                        const clickInfo = click(react.centerX(), react.centerY())
+                        console.log("clickInfo===================", clickInfo)
+                        sleep(1000)
+                    
+                    
+                    
+                        
+                        // 获取输入框
+                        const inputItem = className("android.widget.EditText").findOne(1000)
+                        if(inputItem){  
+                            console.log("inputItem===================", inputItem)
+                            inputItem.click()
+                            // 输入评价 
+                            inputItem.setText(comment)
+                        }else{
+                            maxErrorCount++
+                            back()
+                            continue
+                        }
+                        // 点击好评位置
+                        const goodCommentIconBox = className("android.view.View").descContains("好评").findOne(1000)
+                    
+                        const goodCommentIcon = goodCommentIconBox.child(0)
+
+                        // 获取他的位置信息
+                        const goodCommentIconReact = goodCommentIcon.bounds()
+                        console.log("goodCommentIconReact===================", goodCommentIconReact)
+                        // 模拟点击对应的坐标
+                        click(goodCommentIconReact.centerX() , goodCommentIconReact.centerY() )
+                        // 点击提交
+                        const submit = className("android.view.View").descContains("提交评价").findOne(1000)
+                        console.log("submit===================", submit)
+                        if(submit){
+                            // 找到这个地方的坐标
+                            const submitReact = submit.bounds()
+                            console.log("submitReact===================", submitReact)
+                            // 模拟点击对应的坐标
+                            sleep(1000)
+                            const clickInfo = click(submitReact.centerX() , submitReact.centerY() )
+                            console.log("clickInfo===================", clickInfo)
+                            // 先回到主页
+                            backPageByName('main')
+                            // 再次找到评论列表
+                            findPage('comment')
+                            // 主要正确执行一次就重置所有错误
+                            maxErrorCount = 0
+                          
+                            
+                            break;
+                        }else{
+                            back()
+                            maxErrorCount++
+                            continue
+                        }
+                    } catch (error) {
+                        maxErrorCount++
+                        continue
+                    }
+                    
+                }
+
+            }else{
+                continue
+            }
+            
+            // 通过昵称和标题， 获取对应的商品信息
+            // const goodsInfo = getGoodsInfo(content, title)
+            // console.log("商品信息===================", goodsInfo)
+            // 获取child的text
+            
+        }
+        // 自动评论执行完成， 进入下一个任务
+        console.log('自动评论执行完成， 进入下一个任务')
         
-        console.log('----------coinExchange()-----------')
-        maxLoopMap.coinExchangeRunTime ++
     
     } catch (error) {
         // 记录错误
-        Record.error(`coinExchange, ${error}`)
+        Record.error(`comment, ${error}`)
         // 重新执行
-        findPage('goldCoin')
+        findPage('comment')
 
     }
    
--- src/toolKit/xianyu/service/base.ts
+++ src/toolKit/xianyu/service/base.ts
@@ -7,11 +7,164 @@ export const APPNAME = 'com.taobao.idlefish'
 
 export enum PageType {
     'home'="com.taobao.idlefish.maincontainer.activity.MainActivity",
+    'layout'=" android.widget.FrameLayout",
     'goldCoin'="com.taobao.idlefish.webview.WebHybridActivity",
     "product"='com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostActivity',
     "comment" = "com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostActivity"
 }
 
+/**
+ * 根据页面名称， 返回对应的页面
+ * @param pageName 
+ */
+export function backPageByName(pageName:string){
+    setRunInfo('backPageByName');
+    // 当前页面的信息
+    const name = currentPackage();
+    // 当前页面的activity
+    const activity = currentActivity();
+    if(name !== APPNAME){
+        launch(APPNAME);
+        sleep(1000)
+        const newPageName = currentPackage();
+        console.log('-----name----', newPageName)
+        backPageByName(pageName)
+        return;
+    }
+    console.log('-----activity----', activity, pageName)
+    // 检查页面情况, 找到对应的页面后再执行，寻找页面
+    switch(pageName){
+        case 'main':
+            const myBtn = className("android.widget.FrameLayout").descContains("我的").findOne(1000)
+            if(myBtn){
+                myBtn.click()
+                sleep(1000)
+                console.log('进入我的页面')
+            }else{
+                back();
+                sleep(1000)
+                backPageByName(pageName)
+            }
+           
+            break;
+        case 'home':
+            // 判断当前是否为主页
+            
+            break;
+        case 'goldCoin':
+            if(activity === PageType.goldCoin){
+                console.log('已经进度到金币页面')
+            }else {
+                // 默认是在主页进行路径的查找
+                if(activity === PageType.home || activity === PageType.layout){
+                    console.log('-----PageType.home----')
+
+                    // 找到对应的商品
+                    const myBtn = className("android.widget.FrameLayout").descContains("我的").findOne()
+               
+                    if(myBtn){
+                        myBtn.click()
+                    }
+                    sleep(2000)
+                    //  找到金币入口
+                    const goldButtons =  className("android.view.View").clickable(true).depth(12).find()
+
+                    console.log('-----goldButtons----', goldButtons.length)
+                    for(let i = 0; i < goldButtons.length; i++){
+                        const btn = goldButtons[i]
+                        const react = btn.bounds()
+                        console.log('-----btn----', react.left, react.top, react.right, react.bottom)
+                        console.log('-----btn-children---', btn.childCount())
+                        // 找到后进行点击进入领取金币页面
+                        if(react.left === 23 && react.right ===435){
+                            btn.click()
+                            Record.info('进入金币页面')
+                            setRunInfo('进入金币页面')
+                            sleep(1000)
+                            return;
+                        }
+                    }
+                    // 如果没有命中则再次执行定位页面逻辑
+                    back();
+                    sleep(1000)
+                    backPageByName(pageName)
+                    // findDom(log)
+                }else{
+                    back();
+                    sleep(1000)
+                    backPageByName(pageName)
+                }
+            }
+            break;
+        case 'product':
+            if(activity === PageType.product){
+                console.log('进入商品详情页面')
+            }else {
+                // 默认是在主页进行路径的查找
+                if(activity === PageType.home || activity === PageType.layout){
+                    console.log('-----PageType.home----')
+
+                    // 找到对应的商品
+                    const myBtn = className("android.widget.FrameLayout").descContains("我的").findOne()
+                    console.log('-----myBtn----')
+                    if(myBtn){
+                        myBtn.click()
+                    }
+                    sleep(1000)
+                    //  找到我发布的
+                    const productBtn =  className("android.widget.ImageView").descContains("我发布的").findOne()
+                    if(productBtn){
+                        productBtn.click()
+                    }
+                    sleep(1000)
+                    backPageByName(pageName)
+                }else{
+                    back();
+                    sleep(1000)
+                    backPageByName(pageName)
+                }
+            }
+            break;
+        case 'comment':
+            // 进行检查， 是否是comment, 并且有我卖出的导航
+            if(activity === PageType.comment){
+                const mySoleTab =className("android.view.View").descContains("我卖出的").findOne(1000)
+                if(mySoleTab){
+                    console.log('已经进度到评论页面')
+                }else{
+                    back();
+                    sleep(1000)
+                    backPageByName(pageName) 
+                }
+            }else{
+                if(activity === PageType.home || activity === PageType.layout){
+                    console.log('-----PageType.home----')
+
+                    // 找到对应的商品
+                    const myBtn = className("android.widget.FrameLayout").descContains("我的").findOne()
+                    if(myBtn){
+                        myBtn.click()
+                    }
+                    sleep(1000)
+                    //  找到我的待评价
+                    const commentEntryBtn =  className("android.widget.ImageView").descContains("待评价").findOne()
+                    if(commentEntryBtn){
+                        console.log('进入待评价页面')
+                    }
+                    // findDom(log)
+                }else{
+                    back();
+                    sleep(1000)
+                    backPageByName(pageName)
+                }
+            }
+            break;
+        default:
+            console.log('dddd');
+            
+    }
+   
+}
 // 优化日志
 export const runInfo = {
     log: '开始',
@@ -111,9 +264,8 @@ export const findPage = (pageName:string) =>{
         case 'product':
             if(activity === PageType.product){
                 
-                console.log('进入商品详情页面, 进入新的页面3333')
+                console.log('进入商品详情页面')
                 const btn = className("android.view.View").descContains("今日曝光").findOne(1000)
-                console.log('-----btn----', btn)
                 if(btn){
                     // 执行对应的逻辑
                     setRunInfo('进入商品详情页面')
@@ -124,7 +276,7 @@ export const findPage = (pageName:string) =>{
                
             }else {
                 // 默认是在主页进行路径的查找
-                if(activity === PageType.home){
+                if(activity === PageType.home || activity === PageType.layout){
                     console.log('-----PageType.home----')
 
                     // 找到对应的商品
@@ -138,9 +290,11 @@ export const findPage = (pageName:string) =>{
                     const productBtn =  className("android.widget.ImageView").descContains("我发布的").findOne()
                     if(productBtn){
                         productBtn.click()
+                        sleep(1000)
+                        // 执行曝光逻辑
+                        findDom()
                     }
-                    sleep(1000)
-                    findPage(pageName)
+                   
                     // findDom(log)
                 }else{
                     back();
@@ -155,16 +309,23 @@ export const findPage = (pageName:string) =>{
             if(activity === PageType.comment){
                 const mySoleTab =className("android.view.View").descContains("我卖出的").findOne(1000)
                 if(mySoleTab){
-                    console.log('in comment !!!!!')
-                    // 执行自动品论
-                    startAutoComment()
+                    const mySoleText = mySoleTab.contentDescription +''
+                    if(mySoleText.indexOf("我卖出的 0") > -1){
+                        //没有需要执行的内容了
+                        console.log('没有需要执行的评论')
+                    }else{
+                        console.log('in comment !!!!!')
+                        // 执行自动品论
+                        startAutoComment()
+                    }
+                   
                 }else{
                     back();
                     sleep(1000)
                     findPage(pageName) 
                 }
             }else{
-                if(activity === PageType.home){
+                if(activity === PageType.home || activity === PageType.layout){
                     console.log('-----PageType.home----')
 
                     // 找到对应的商品
@@ -211,91 +372,98 @@ export const xyBaseRun = () =>{
     // 运行对应的收集数据代码
     // findDom();
     // 执行曝光， 先找到对应的页面，然后执行对应的逻辑
-    var time = setInterval(() => {
+    // var time = setInterval(() => {
 
-    }, 2000);
-    threads.start(function () {
-        var time = setInterval(() => {
+    // }, 2000);
+    // threads.start(function () {
+    //     var time = setInterval(() => {
            
-        }, 2000);
-        var window = floaty.window(
-            `<vertical>
-                <button id="center"  margin="0" w="60">页面信息</button>
-                <button id="start"  margin="0" w="60">尝试</button>
+    //     }, 2000);
+    //     var window = floaty.window(
+    //         `<vertical>
+    //             <button id="center"  margin="0" w="60">页面信息</button>
+    //             <button id="start"  margin="0" w="60">尝试</button>
         
-                <button id="exit"   margin="0" w="60">退出</button>
-                <text id="runLog"  padding="10 5 10 5" bg="#ff0000" w="300" h="auto" text="Hello" />
-            </vertical>` as any
-        );
-        window.setPosition(window.getX(), window.getY() + 200);
-           var x = 0,
-            y = 0,
-            windowX = 0,
-            windowY = 0,
-            isRuning = false,
-            showConsole = false,
-            isShowingAll = true;
+    //             <button id="exit"   margin="0" w="60">退出</button>
+    //             <text id="runLog"  padding="10 5 10 5" bg="#ff0000" w="300" h="auto" text="Hello" />
+    //         </vertical>` as any
+    //     );
+    //     window.setPosition(window.getX(), window.getY() + 100);
+    //        var x = 0,
+    //         y = 0,
+    //         windowX = 0,
+    //         windowY = 0,
+    //         isRuning = false,
+    //         showConsole = false,
+    //         isShowingAll = true;
     
-        window.center.setOnTouchListener(function (view, event) {
-            switch (event.getAction()) {
-                case event.ACTION_DOWN:
-                    x = event.getRawX();
-                    y = event.getRawY();
-                    windowX = window.getX();
-                    windowY = window.getY();
-                    break;
-                case event.ACTION_MOVE:
-                    window.setPosition(windowX + (event.getRawX() - x), windowY + (event.getRawY() - y));
-                    break;
-                case event.ACTION_UP:
-                    // 获取当前页面的信息，并且打印出来
-                    try {
-                        // Record.info(`currentActivity: ${currentActivity()}`)
-                        // console.log('currentActivity', currentActivity())
-                        // const name = currentPackage();
-                        // Record.info(`currentPackage: ${name}`)
-                    } catch (error) {
-                        // Record.error(error?.message)
-                        console.log(error?.message)
-                    }
+    //     window.center.setOnTouchListener(function (view, event) {
+    //         switch (event.getAction()) {
+    //             case event.ACTION_DOWN:
+    //                 x = event.getRawX();
+    //                 y = event.getRawY();
+    //                 windowX = window.getX();
+    //                 windowY = window.getY();
+    //                 break;
+    //             case event.ACTION_MOVE:
+    //                 window.setPosition(windowX + (event.getRawX() - x), windowY + (event.getRawY() - y));
+    //                 break;
+    //             case event.ACTION_UP:
+    //                 // 获取当前页面的信息，并且打印出来
+    //                 try {
+    //                     // Record.info(`currentActivity: ${currentActivity()}`)
+    //                     // console.log('currentActivity', currentActivity())
+    //                     // const name = currentPackage();
+    //                     // Record.info(`currentPackage: ${name}`)
+    //                 } catch (error) {
+    //                     // Record.error(error?.message)
+    //                     console.log(error?.message)
+    //                 }
                    
-                    break;
-            }
-            return true;
-        });
+    //                 break;
+    //         }
+    //         return true;
+    //     });
         
-        window.exit.click(function () {
-            window.close();
-        });
-        window.start.click(function () {
-           try {
-             getPageInfo()
+    //     window.exit.click(function () {
+    //         window.close();
+    //     });
+    //     window.start.click(function () {
+    //        try {
+    //          getPageInfo()
           
            
-        // 搜索完后要进行页面滚动
+    //     // 搜索完后要进行页面滚动
             
-           } catch (error) {
-               console.log(error)
-           }
-        });
+    //        } catch (error) {
+    //            console.log(error)
+    //        }
+    //     });
 
         
-        // 初始化
-        initRunInfo(window)
-        // 进入获取金币页面
-        findPage('comment')
-        // // 执行获取金币的逻辑
-        // coinExchange()
-        // 进入商品详情页面
-        // findPage('product')
-        // 执行曝光逻辑
-        // findDom()
+    //     // 初始化
+    //     initRunInfo(window)
+    //     // 进入获取金币页面
+    //     findPage('comment')
+    //     // // 执行获取金币的逻辑
+    //     // coinExchange()
+    //     // 进入商品详情页面
+    //     // findPage('product')
+    //     // 执行曝光逻辑
+    //     // findDom()
 
-        // 到达金币页面执行金币逻辑
+    //     // 到达金币页面执行金币逻辑
 
-        // 进入特定页面执行对应的逻辑
+    //     // 进入特定页面执行对应的逻辑
        
-    }); 
+    // }); 
+
+    // 执行曝光逻辑
+    findPage('comment')
+    // 执行金币逻辑
+    findPage('goldCoin')
+    // 执行商品详情逻辑
+    findPage('product')
 
 }
 
